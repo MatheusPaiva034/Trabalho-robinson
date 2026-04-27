35c034a1dcd63f67af6fd45ee2dece95bb5f4baf
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:32:24 -0300

    Add files via upload

---
+++ Exercício  7 -  Cadastro de Filmes/Filme.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_07
+{
+    internal class Filme
+    {
+        public string Nome { get; private set; }
+        public string Genero { get; private set; }
+        public int Duracao { get; private set; }
+        public int Classificacao { get; private set; }
+
+        
+        public Filme(string nome, string genero, int duracao, int classificacao)
+        {
+            
+            if (string.IsNullOrWhiteSpace(nome))
+                Nome = "Inválido";
+            else
+                Nome = nome;
+
+            if (string.IsNullOrWhiteSpace(genero))
+                Genero = "Inválido";
+            else
+                Genero = genero;
+
+            if (duracao <= 0)
+                Duracao = 0;
+            else
+                Duracao = duracao;
+
+            if (classificacao < 0)
+                Classificacao = 0;
+            else
+                Classificacao = classificacao;
+        }
+        public void ExibirResumo()
+        {
+            Console.WriteLine("----- FILME -----");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Gênero: {Genero}");
+            Console.WriteLine($"Duração: {Duracao} min");
+            Console.WriteLine($"Classificação: {Classificacao}+");
+            Console.WriteLine();
+        }
+        public bool EhPermitido(int idade)
+        {
+            return idade >= Classificacao;
+        }
+        public void AlterarDuracao(int novaDuracao)
+        {
+            if (novaDuracao > 0)
+            {
+                Duracao = novaDuracao;
+            }
+            else
+            {
+                Console.WriteLine("Duração inválida!");
+            }
+        }
+    }
+}
---
+++ Exercício  7 -  Cadastro de Filmes/Program.cs
@@ -0,0 +1,13 @@
+﻿using Atividade_07;
+
+Filme f1 = new Filme("Interestelar", "Ficção Científica", 169, 12);
+Filme f2 = new Filme("", "", -30, -5);
+
+f1.ExibirResumo();
+f2.ExibirResumo();
+
+Console.WriteLine(f1.EhPermitido(10)); 
+Console.WriteLine(f1.EhPermitido(15)); 
+
+f2.AlterarDuracao(120);
+f2.ExibirResumo();
