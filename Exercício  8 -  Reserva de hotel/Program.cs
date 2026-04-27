8ec04804fe92a91646953986c8792935fce7eab9
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:34:02 -0300

    Add files via upload

---
+++ Exercício  8 -  Reserva de hotel/Program.cs
@@ -0,0 +1,19 @@
+﻿using Atividade_08;
+
+ReservaHotel r1 = new ReservaHotel("Carlos", 12);
+ReservaHotel r2 = new ReservaHotel("", -8, 0, -300);
+
+r1.ExibirReserva();
+r2.ExibirReserva();
+
+Console.WriteLine(r1.CalcularTotal());
+Console.WriteLine(r2.CalcularTotal());
+
+r1.AdicionarDiarias(2);
+r1.ExibirReserva();
+
+r2.CancelarReserva();
+r2.ExibirReserva();
+
+r2.ReativarReserva();
+r2.ExibirReserva();
---
+++ Exercício  8 -  Reserva de hotel/ReservaHotel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_08
+{
+    internal class ReservaHotel
+    {
+        public string NomeHospede { get; private set; }
+        public int NumeroQuarto { get; private set; }
+        public int QuantidadeDiarias { get; private set; }
+        public double ValorDiaria { get; private set; }
+        public string Status { get; private set; }
+
+     
+        public ReservaHotel(string nomeHospede, int numeroQuarto)
+        {
+            NomeHospede = string.IsNullOrWhiteSpace(nomeHospede) ? "Hóspede não informado" : nomeHospede;
+            NumeroQuarto = numeroQuarto <= 0 ? 1 : numeroQuarto;
+
+            QuantidadeDiarias = 1;
+            ValorDiaria = 50;
+            Status = "Ativa";
+        }
+
+        public ReservaHotel(string nomeHospede, int numeroQuarto, int quantidadeDiarias, double valorDiaria)
+        {
+            NomeHospede = string.IsNullOrWhiteSpace(nomeHospede) ? "Hóspede não informado" : nomeHospede;
+            NumeroQuarto = numeroQuarto <= 0 ? 1 : numeroQuarto;
+            QuantidadeDiarias = quantidadeDiarias <= 0 ? 1 : quantidadeDiarias;
+            ValorDiaria = valorDiaria <= 0 ? 50 : valorDiaria;
+
+            Status = "Ativa";
+        }
+
+
+        public double CalcularTotal()
+        {
+            return QuantidadeDiarias * ValorDiaria;
+        }
+
+   
+        public void ExibirReserva()
+        {
+            Console.WriteLine("----- RESERVA -----");
+            Console.WriteLine($"Hóspede: {NomeHospede}");
+            Console.WriteLine($"Quarto: {NumeroQuarto}");
+            Console.WriteLine($"Diárias: {QuantidadeDiarias}");
+            Console.WriteLine($"Valor diária: R$ {ValorDiaria}");
+            Console.WriteLine($"Total: R$ {CalcularTotal()}");
+            Console.WriteLine($"Status: {Status}");
+            Console.WriteLine();
+        }
+
+     
+        public void AdicionarDiarias(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                QuantidadeDiarias += quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida!");
+            }
+        }
+
+        public void CancelarReserva()
+        {
+            Status = "Cancelada";
+        }
+        public void ReativarReserva()
+        {
+            Status = "Ativa";
+        }
+    }
+}
