b60c8a6e063e4d38110e6aa3f628881581c42118
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:26:09 -0300

    Add files via upload

---
+++ Exercício 1 – Classe Aluno e Notas/Exercício 1 – Classe Aluno e Notas/Aluno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Aluno
+{
+    // Atributos
+    public string RA;
+    public string Nome;
+    public double NotaProva;
+    public double NotaTrabalho;
+    public double NotaFinal;
+
+    // Método para receber dados
+    public void ReceberDados()
+    {
+        Console.Write("Digite o RA: ");
+        RA = Console.ReadLine();
+
+        Console.Write("Digite o Nome: ");
+        Nome = Console.ReadLine();
+
+        Console.Write("Digite a Nota da Prova: ");
+        NotaProva = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a Nota do Trabalho: ");
+        NotaTrabalho = double.Parse(Console.ReadLine());
+    }
+
+    // Calcula média
+    public void CalcularMedia()
+    {
+        NotaFinal = (NotaProva + NotaTrabalho) / 2;
+    }
+
+    // Calcula situação do aluno
+    public bool CalcularNotaFinal()
+    {
+        double mediaMinima = 6.0;
+
+        if (NotaFinal >= mediaMinima)
+        {
+            Console.WriteLine("Aluno aprovado direto!");
+            return true;
+        }
+        else
+        {
+            double notaNecessaria = mediaMinima - NotaFinal;
+            Console.WriteLine("Aluno precisa de prova final.");
+            Console.WriteLine("Precisa tirar pelo menos: " + notaNecessaria.ToString("F2"));
+            return false;
+        }
+    }
+
+    // Mostra a média
+    public void ImprimirNotaFinal()
+    {
+        Console.WriteLine("Nota final (média): " + NotaFinal.ToString("F2"));
+    }
+}
---
+++ Exercício 1 – Classe Aluno e Notas/Exercício 1 – Classe Aluno e Notas/Exercício 1 – Classe Aluno e Notas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno();
+
+        aluno.ReceberDados();
+        aluno.CalcularMedia();
+        aluno.ImprimirNotaFinal();
+        aluno.CalcularNotaFinal();
+
+        Console.ReadKey();
+    }
+}
